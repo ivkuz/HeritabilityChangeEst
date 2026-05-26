564b17dc81257f381da0382c1c1c67ca6fab8706
Ivan Kuznetsov <kuznetsi@login2.hpc.ut.ee>
2026-05-26 14:52:26 +0300

    figures_R2_S4_S7.R - plotting by settlement type added

--- figures_R2_S4_S7.R
+++ figures_R2_S4_S7.R
@@ -1,6 +1,7 @@
 
 ###############################################################################
-# Plot Fig. S4 and S7, Untransmitted, sex-specific, cognitive/non-cognitive ###
+# Plot Fig. S4 and S7, Untransmitted, sex-specific, ###########################
+# cognitive/non-cognitive, settlement type ####################################
 # Trait variance explained by PGS in the post-Soviet and Soviet groups ########
 ###############################################################################
 
@@ -319,6 +320,82 @@ for(sex in c("male", "female")){
               row.names = F, quote = F, sep = "\t")
   
   pdf(paste0("~/EA_heritability/figures/paper/revision/r2_", sex, ".pdf"), width=5.5, height=6)
+  print(
+    grid.arrange(
+      grobs = plot_list_inc,
+      layout_matrix = matrix(1:8, ncol = 2, byrow = T),
+      widths = c(1, 1.5)
+    )
+  )
+  
+  grid.text("a", x = 0.02, y = 0.98, gp = gpar(fontsize=14, fontface = "bold"))
+  grid.text("b", x = 0.42, y = 0.98, gp = gpar(fontsize=14, fontface = "bold"))
+  grid.text("c", x = 0.02, y = 0.73, gp = gpar(fontsize=14, fontface = "bold"))
+  grid.text("d", x = 0.42, y = 0.73, gp = gpar(fontsize=14, fontface = "bold"))
+  grid.text("e", x = 0.02, y = 0.48, gp = gpar(fontsize=14, fontface = "bold"))
+  grid.text("f", x = 0.42, y = 0.48, gp = gpar(fontsize=14, fontface = "bold"))
+  grid.text("g", x = 0.02, y = 0.23, gp = gpar(fontsize=14, fontface = "bold"))
+  grid.text("h", x = 0.42, y = 0.23, gp = gpar(fontsize=14, fontface = "bold"))
+  
+  print(
+    grid.arrange(
+      grobs = plot_list,
+      layout_matrix = matrix(1:8, ncol = 2, byrow = T),
+      widths = c(1, 1.5)
+    )
+  )
+  
+  
+  grid.text("a", x = 0.02, y = 0.98, gp = gpar(fontsize=14, fontface = "bold"))
+  grid.text("b", x = 0.42, y = 0.98, gp = gpar(fontsize=14, fontface = "bold"))
+  grid.text("c", x = 0.02, y = 0.73, gp = gpar(fontsize=14, fontface = "bold"))
+  grid.text("d", x = 0.42, y = 0.73, gp = gpar(fontsize=14, fontface = "bold"))
+  grid.text("e", x = 0.02, y = 0.48, gp = gpar(fontsize=14, fontface = "bold"))
+  grid.text("f", x = 0.42, y = 0.48, gp = gpar(fontsize=14, fontface = "bold"))
+  grid.text("g", x = 0.02, y = 0.23, gp = gpar(fontsize=14, fontface = "bold"))
+  grid.text("h", x = 0.42, y = 0.23, gp = gpar(fontsize=14, fontface = "bold"))
+  
+  
+  dev.off()
+  
+}
+
+
+# By settlement type
+for(settlement in c("rural", "town", "city")){
+  
+  r2_res_v <- paste0("r2_adj_", c("EduYears", "OS", "Height_first", "BMI_first"), "_", settlement, ".tsv")
+  bootstrap_v <- paste0("r2_adj_", c("EduYears", "OS", "Height_first", "BMI_first"), "_", settlement, "_1000.tsv")
+  titles <- c("EA", "OS", "Height", "BMI")
+  lim_list_inc <- c(0.17, 0.14, 0.25, 0.17)
+  steps_inc = c(0.05, 0.05, 0.1, 0.05)
+  lim_list <- c(0.18, 0.14, 0.5, 0.19)
+  steps = c(0.05, 0.05, 0.1, 0.05)
+  
+  
+  plot_list_inc <- list()
+  plot_list <- list()
+  pval_dt <- data.table()
+  for(i in 1:4){
+    
+    r2_res <- fread(paste0("~/EA_heritability/results/revision/", r2_res_v[i]))
+    bootstrap <- fread(paste0("~/EA_heritability/results/revision/", bootstrap_v[i]))
+    plot_list_inc <- append(plot_list_inc, makeR2(r2_res = r2_res, bootstrap = bootstrap, 
+                                                  r2_var = "r2_inc", title = titles[i], 
+                                                  lim = lim_list_inc[i], step = steps_inc[i]))
+    plot_list <- append(plot_list, makeR2(r2_res = r2_res, bootstrap = bootstrap, 
+                                          r2_var = "r2", title = titles[i], 
+                                          lim = lim_list[i], step = steps[i]))
+    pval_dt <- rbind(pval_dt, compareR2(r2_res = r2_res, bootstrap = bootstrap))
+    
+  }
+  
+  pval_dt <- as.data.table(pval_dt)
+  pval_dt[, trait := rep(titles, each = 2)]
+  write.table(pval_dt, paste0("~/EA_heritability/figures/paper/revision/r2_pval_", settlement, ".tsv"),
+              row.names = F, quote = F, sep = "\t")
+  
+  pdf(paste0("~/EA_heritability/figures/paper/revision/r2_", settlement, ".pdf"), width=5.5, height=6)
   print(
     grid.arrange(
       grobs = plot_list_inc,
